15f5214533c66e8bbb92b7a2daee9ba1585c289b
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-03-12 20:43:08 +0000

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+//
+var gulp    = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task('default', ['server']);
+
+// npm install supervisor -g
+gulp.task('server', function () {
+  return gulp.src('').pipe(shell([ 'node-supervisor app.js' ]));
+});
+
+gulp.task('open', function() {
+  return gulp.src('').
+    pipe(shell("open https://github.com/crguezl/expressloadfileontotextarea"));
+});
